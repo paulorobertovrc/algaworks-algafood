8824f4abce97bd162012b1f33a6fac147f33de4e
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-02-10 09:57:02 -0300

    Projeto inicial

---
+++ src/main/java/br/dev/pauloroberto/algafood/AlgaworksAlgafoodApplication.java
@@ -0,0 +1,13 @@
+package br.dev.pauloroberto.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksAlgafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksAlgafoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/pauloroberto/algafood/AlgaworksAlgafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.pauloroberto.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksAlgafoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
