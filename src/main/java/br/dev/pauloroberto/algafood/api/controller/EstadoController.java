6ad268878da5b38e7a40929981298716e6fdc517
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-02-12 11:14:55 -0400

    Implementação do EstadoController e método listar

---
+++ src/main/java/br/dev/pauloroberto/algafood/api/controller/EstadoController.java
@@ -0,0 +1,22 @@
+package br.dev.pauloroberto.algafood.api.controller;
+
+import br.dev.pauloroberto.algafood.domain.model.Estado;
+import br.dev.pauloroberto.algafood.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return estadoRepository.listar();
+    }
+}
