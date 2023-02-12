5cfdb2bb09334c9abc8bc97145f0116f62574d43
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-02-12 11:17:36 -0400

    Pequenas alterações

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
-INSERT INTO cozinha (nome) VALUES ('Brasileira');
-INSERT INTO cozinha (nome) VALUES ('Tailandesa');
-INSERT INTO cozinha (nome) VALUES ('Indiana');
+INSERT INTO cozinha (id, nome) VALUES (1, 'Brasileira');
+INSERT INTO cozinha (id, nome) VALUES (2, 'Tailandesa');
+INSERT INTO cozinha (id, nome) VALUES (3, 'Indiana');
+
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Delivery', 10, 2);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Sabor Brasil', 10, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Tuk Tuk Comida Indiana', 10, 3);
