e755548d281f8ea6019c773191ca8bc4a1083e17
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-02-13 08:22:14 -0400

    Implementação dos métodos atualizar e remover

--- src/main/java/br/dev/pauloroberto/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/dev/pauloroberto/algafood/api/controller/CozinhaController.java
@@ -1,7 +1,10 @@
 package br.dev.pauloroberto.algafood.api.controller;
 
+import br.dev.pauloroberto.algafood.domain.exception.EntidadeEmUsoException;
+import br.dev.pauloroberto.algafood.domain.exception.EntidadeNaoEncontradaException;
 import br.dev.pauloroberto.algafood.domain.model.Cozinha;
 import br.dev.pauloroberto.algafood.domain.repository.CozinhaRepository;
+import br.dev.pauloroberto.algafood.domain.service.CadastroCozinhaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,8 @@ import java.util.List;
 public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
+    @Autowired
+    private CadastroCozinhaService cadastroCozinhaService;
 
     @GetMapping
     public List<Cozinha> listar() {
@@ -34,6 +39,34 @@ public class CozinhaController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-        return cozinhaRepository.salvar(cozinha);
+        return cadastroCozinhaService.salvar(cozinha);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinhaAtualizada) {
+        Cozinha cozinha = cozinhaRepository.buscar(id);
+
+        if (cozinha != null) {
+            cozinha.setNome(cozinhaAtualizada.getNome());
+            cozinhaRepository.salvar(cozinha);
+
+            return ResponseEntity.ok(cozinha);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
+        try {
+            cadastroCozinhaService.excluir(id);
+            return ResponseEntity.noContent().build();
+
+        } catch (EntidadeEmUsoException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
