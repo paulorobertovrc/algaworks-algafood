f8e0da5b7b6155298083e6dae4b0fe9691910bdd
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-02-17 09:38:43 -0400

    Criação das entidades de domínio e migration V005

---
+++ src/main/java/br/dev/pauloroberto/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,31 @@
+package br.dev.pauloroberto.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private Integer quantidade;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+}
---
+++ src/main/java/br/dev/pauloroberto/algafood/domain/model/StatusPedido.java
@@ -0,0 +1,8 @@
+package br.dev.pauloroberto.algafood.domain.model;
+
+public enum StatusPedido {
+    CRIADO,
+    CONFIRMADO,
+    ENTREGUE,
+    CANCELADO
+}
