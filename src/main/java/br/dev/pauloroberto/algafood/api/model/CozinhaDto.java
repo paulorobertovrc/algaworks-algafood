be60858c059b2243d890fb8f4a22f7b4b7eb678e
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-02-25 21:09:53 -0300

    Primeira implementação do padrão DTO

---
+++ src/main/java/br/dev/pauloroberto/algafood/api/model/CozinhaDto.java
@@ -0,0 +1,11 @@
+package br.dev.pauloroberto.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CozinhaDto {
+    public Long id;
+    public String nome;
+}
---
+++ src/main/java/br/dev/pauloroberto/algafood/api/model/RestauranteDto.java
@@ -0,0 +1,15 @@
+package br.dev.pauloroberto.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+public class RestauranteDto {
+    private Long id;
+    private String nome;
+    private BigDecimal taxaFrete;
+    private CozinhaDto cozinha;
+}
