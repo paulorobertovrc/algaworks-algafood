cd5bf4fec3e2da4c20304658cdd03cd948b27888
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-03-24 08:47:59 -0400

    Servindo arquivos de fotos pela API

--- src/main/java/br/dev/pauloroberto/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/br/dev/pauloroberto/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -3,17 +3,24 @@ package br.dev.pauloroberto.algafood.api.controller;
 import br.dev.pauloroberto.algafood.api.assembler.FotoProdutoDtoAssembler;
 import br.dev.pauloroberto.algafood.api.model.FotoProdutoDto;
 import br.dev.pauloroberto.algafood.api.model.input.FotoProdutoInputDto;
+import br.dev.pauloroberto.algafood.domain.exception.EntidadeNaoEncontradaException;
 import br.dev.pauloroberto.algafood.domain.model.FotoProduto;
 import br.dev.pauloroberto.algafood.domain.model.Produto;
 import br.dev.pauloroberto.algafood.domain.service.CadastroProdutoService;
 import br.dev.pauloroberto.algafood.domain.service.CatalogoFotoProdutoService;
+import br.dev.pauloroberto.algafood.domain.service.FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
@@ -24,6 +31,8 @@ public class RestauranteProdutoFotoController {
     private CadastroProdutoService cadastroProdutoService;
     @Autowired
     private FotoProdutoDtoAssembler fotoProdutoDtoAssembler;
+    @Autowired
+    private FotoStorageService fotoStorageService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public FotoProdutoDto buscar(@PathVariable Long produtoId,
@@ -31,6 +40,40 @@ public class RestauranteProdutoFotoController {
         FotoProduto fotoProduto = catalogoFotoProdutoService.verificarSeExiste(produtoId, restauranteId);
 
         return fotoProdutoDtoAssembler.toDto(fotoProduto);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long produtoId,
+                                                          @PathVariable Long restauranteId,
+                                                          @RequestHeader(name = "accept") String acceptHeader)
+            throws HttpMediaTypeNotAcceptableException {
+        try {
+            FotoProduto fotoProduto = catalogoFotoProdutoService.verificarSeExiste(produtoId, restauranteId);
+
+            MediaType mediaType = MediaType.parseMediaType(fotoProduto.getContentType());
+            List<MediaType> mediaTypesAceitas = MediaType.parseMediaTypes(acceptHeader);
+
+            verificarCompatibilidadeMediaType(mediaType, mediaTypesAceitas);
+
+            InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo()).getInputStream();
+
+            return ResponseEntity.ok()
+                    .contentType(mediaType)
+                    .body(new InputStreamResource(inputStream));
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private void verificarCompatibilidadeMediaType(MediaType mediaType, List<MediaType> mediaTypesAceitas)
+            throws HttpMediaTypeNotAcceptableException {
+        boolean compativel = mediaTypesAceitas.stream()
+                .anyMatch(mediaTypeAceita -> mediaTypeAceita.isCompatibleWith(mediaType));
+
+        if (!compativel) {
+            throw new HttpMediaTypeNotAcceptableException(mediaTypesAceitas);
+        }
     }
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
