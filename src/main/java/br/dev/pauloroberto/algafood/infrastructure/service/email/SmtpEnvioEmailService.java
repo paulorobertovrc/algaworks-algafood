8bf506bcb2ad0eae3df898fcb90344bae2ac8221
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-03-28 21:21:12 -0400

    Inclusão da dependência Apache FreeMarker para utilizar templates de corpo de e-mails

--- src/main/java/br/dev/pauloroberto/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/br/dev/pauloroberto/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -2,10 +2,13 @@ package br.dev.pauloroberto.algafood.infrastructure.service.email;
 
 import br.dev.pauloroberto.algafood.core.email.EmailProperties;
 import br.dev.pauloroberto.algafood.domain.service.EnvioEmailService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -16,17 +19,21 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
     private JavaMailSender mailSender;
     @Autowired
     private EmailProperties emailProperties;
+    @Autowired
+    private Configuration freemarkerConfig;
 
     @Override
     public void enviar(Mensagem mensagem) {
         try {
+            String corpo = processarTemplate(mensagem);
+
             MimeMessage mimeMessage = mailSender.createMimeMessage();
 
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
             helper.setFrom(emailProperties.getRemetente());
             helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
             helper.setSubject(mensagem.getAssunto());
-            helper.setText(mensagem.getCorpo(), true);
+            helper.setText(corpo, true);
 
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
@@ -36,6 +43,16 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 
     @Override
     public void enviar(Mensagem mensagem, String remetente) {
+    }
+
+    private String processarTemplate(Mensagem mensagem) {
+        try {
+            Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
+
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template, mensagem.getVariaveis());
+        } catch (Exception e) {
+            throw new EmailException("Não foi possível montar o template do e-mail", e);
+        }
     }
 
 }
