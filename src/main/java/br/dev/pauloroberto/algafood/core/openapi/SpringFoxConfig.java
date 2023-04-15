bcc0a81424a271cd65c5b80674035d348dcd8376
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-04-15 16:48:55 -0400

    Aplicação de filtros para exibição apenas de endpoints específicos

--- src/main/java/br/dev/pauloroberto/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/dev/pauloroberto/algafood/core/openapi/SpringFoxConfig.java
@@ -2,6 +2,7 @@ package br.dev.pauloroberto.algafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -12,7 +13,10 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.dev.pauloroberto.algafood.api")) // Exibe apenas os endpoints de um pacote específico
+//                .paths(PathSelectors.ant("/restaurantes/*")) // Este filtro exibe apenas os endpoints que começam com /restaurantes/
+//                .paths(PathSelectors.ant("/restaurantes/**")) // Este filtro exibe apenas os endpoints que começam com /restaurantes/ e terminam com qualquer coisa
+                .paths(PathSelectors.any()) // Este filtro exibe todos os endpoints
                 .build();
     }
 
