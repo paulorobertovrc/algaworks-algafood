f21b6c415c6df6e88c05a5650880ef06f4b2b334
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-04-15 16:29:53 -0400

    Inclusão da biblioteca SpringFox e implementação da configuração inicial

--- src/main/java/br/dev/pauloroberto/algafood/AlgaworksAlgafoodApplication.java
+++ src/main/java/br/dev/pauloroberto/algafood/AlgaworksAlgafoodApplication.java
@@ -4,11 +4,13 @@ import br.dev.pauloroberto.algafood.infrastructure.repository.CustomJpaRepositor
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
+@EnableWebMvc
 public class AlgaworksAlgafoodApplication {
 
 	public static void main(String[] args) {
