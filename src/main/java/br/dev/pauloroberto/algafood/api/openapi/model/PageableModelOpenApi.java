7ea5de801e7d26cdea798bcabd2905435e02ef54
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-04-18 07:23:00 -0400

    Aprimoramento da documentação de objetos paginados

---
+++ src/main/java/br/dev/pauloroberto/algafood/api/openapi/model/PageableModelOpenApi.java
@@ -0,0 +1,23 @@
+package br.dev.pauloroberto.algafood.api.openapi.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@ApiModel("Pageable")
+public class PageableModelOpenApi {
+    @ApiModelProperty(example = "0", value = "Número da página (começa em 0)")
+    private int page;
+
+    @ApiModelProperty(example = "10", value = "Quantidade de registros por página")
+    private int size;
+
+    @ApiModelProperty(example = "nome,asc", value = "Nome da propriedade e sentido para ordenação [asc, desc]. "
+            + "Pode ser passado mais de um critério de ordenação separando por vírgula.")
+    private List<String> sort;
+}
