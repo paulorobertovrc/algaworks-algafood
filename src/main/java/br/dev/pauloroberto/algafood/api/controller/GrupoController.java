f2cd74a301e87c5f5c7f8516751fdab3f9dd3486
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-04-18 06:22:53 -0400

    Descrição do media type da resposta dos endpoints

--- src/main/java/br/dev/pauloroberto/algafood/api/controller/GrupoController.java
+++ src/main/java/br/dev/pauloroberto/algafood/api/controller/GrupoController.java
@@ -11,13 +11,14 @@ import br.dev.pauloroberto.algafood.domain.model.Grupo;
 import br.dev.pauloroberto.algafood.domain.service.CadastroGrupoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/grupos")
+@RequestMapping(path = "/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GrupoController implements GrupoControllerOpenApi {
     @Autowired
     private CadastroGrupoService cadastroGrupoService;
