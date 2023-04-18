65d0a4085e2fea48196f902212a007cf66b065af
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-04-18 07:22:00 -0400

    Desacoplamento das anotações do controlador de Cozinha

--- src/main/java/br/dev/pauloroberto/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/dev/pauloroberto/algafood/api/controller/CozinhaController.java
@@ -4,16 +4,16 @@ import br.dev.pauloroberto.algafood.api.assembler.CozinhaDomainObjectAssembler;
 import br.dev.pauloroberto.algafood.api.assembler.CozinhaDtoAssembler;
 import br.dev.pauloroberto.algafood.api.model.CozinhaDto;
 import br.dev.pauloroberto.algafood.api.model.input.CozinhaInputDto;
+import br.dev.pauloroberto.algafood.api.openapi.controller.CozinhaControllerOpenApi;
 import br.dev.pauloroberto.algafood.domain.model.Cozinha;
 import br.dev.pauloroberto.algafood.domain.repository.CozinhaRepository;
 import br.dev.pauloroberto.algafood.domain.service.CadastroCozinhaService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +22,8 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/cozinhas")
-@Api(tags = "Cozinhas")
-public class CozinhaController {
+@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CozinhaController implements CozinhaControllerOpenApi {
     @Autowired
     private CozinhaRepository cozinhaRepository;
     @Autowired
@@ -35,7 +34,6 @@ public class CozinhaController {
     private CozinhaDomainObjectAssembler cozinhaDomainObjectAssembler;
 
     @GetMapping
-    @ApiOperation("Lista as cozinhas")
     public Page<CozinhaDto> listar(Pageable pageable) {
         Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
         List<CozinhaDto> cozinhasDto = cozinhaDtoAssembler.toDtoList(cozinhasPage.getContent());
@@ -44,7 +42,6 @@ public class CozinhaController {
     }
 
     @GetMapping("/{id}")
-    @ApiOperation("Busca uma cozinha por ID")
     public CozinhaDto buscar(@PathVariable Long id) {
         Cozinha cozinha = cadastroCozinhaService.verificarSeExiste(id);
 
@@ -52,7 +49,6 @@ public class CozinhaController {
     }
 
     @GetMapping("/por-nome")
-    @ApiOperation("Busca uma cozinha por nome")
     public ResponseEntity<List<CozinhaDto>> buscarTodasPorNome(@RequestParam String nome) {
         List<CozinhaDto> cozinhas = cozinhaDtoAssembler.toDtoList(cozinhaRepository.findTodasByNomeContaining(nome));
 
@@ -60,7 +56,6 @@ public class CozinhaController {
     }
 
     @GetMapping("/por-nome-exato")
-    @ApiOperation("Busca uma cozinha por nome exato")
     public ResponseEntity<CozinhaDto> buscarPorNomeExato(@RequestParam String nome) {
         Optional<Cozinha> cozinha = cozinhaRepository.findByNome(nome);
 
@@ -70,14 +65,12 @@ public class CozinhaController {
     }
 
     @GetMapping("/existe-por-nome")
-    @ApiOperation("Verifica se existe uma cozinha com o nome informado")
     public ResponseEntity<Boolean> existePorNome(@RequestParam String nome) {
         return ResponseEntity.ok(cozinhaRepository.existsByNomeContaining(nome));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation("Cadastra uma cozinha")
     public CozinhaDto adicionar(@RequestBody @Valid CozinhaInputDto cozinhaInput) {
         Cozinha cozinha = cozinhaDomainObjectAssembler.toDomainObject(cozinhaInput);
 
@@ -85,7 +78,6 @@ public class CozinhaController {
     }
 
     @PutMapping("/{id}")
-    @ApiOperation("Atualiza uma cozinha")
     public CozinhaDto atualizar(@PathVariable Long id,
                              @RequestBody @Valid CozinhaInputDto cozinhaInput) {
         Cozinha cozinha = cadastroCozinhaService.verificarSeExiste(id);
@@ -97,7 +89,6 @@ public class CozinhaController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ApiOperation("Remove uma cozinha")
     public void remover(@PathVariable Long id) {
         cadastroCozinhaService.excluir(id);
     }
---
+++ src/main/java/br/dev/pauloroberto/algafood/api/openapi/controller/CozinhaControllerOpenApi.java
@@ -0,0 +1,104 @@
+package br.dev.pauloroberto.algafood.api.openapi.controller;
+
+import br.dev.pauloroberto.algafood.api.exception.handler.Problem;
+import br.dev.pauloroberto.algafood.api.model.CozinhaDto;
+import br.dev.pauloroberto.algafood.api.model.input.CozinhaInputDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Api(tags = "Cozinhas")
+public interface CozinhaControllerOpenApi {
+    @ApiOperation("Lista as cozinhas")
+    Page<CozinhaDto> listar(Pageable pageable);
+
+    @ApiOperation("Busca uma cozinha por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "ID da cozinha inválido", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Cozinha não encontrada", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            ))
+    })
+    CozinhaDto buscar(@ApiParam(value = "ID de uma cozinha", example = "1") Long id);
+
+    @ApiOperation("Busca uma cozinha por nome")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "Nome da cozinha inválido", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Cozinha não encontrada", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            ))
+    })
+    ResponseEntity<List<CozinhaDto>> buscarTodasPorNome(@ApiParam(value = "Nome de uma cozinha", example = "Brasileira")
+                                                        String nome);
+
+    @ApiOperation("Busca uma cozinha por nome exato")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "Nome da cozinha inválido", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Cozinha não encontrada", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            ))
+    })
+    ResponseEntity<CozinhaDto> buscarPorNomeExato(@ApiParam(value = "Nome de uma cozinha", example = "Brasileira")
+                                                  String nome);
+
+    @ApiOperation("Verifica se existe uma cozinha com o nome informado")
+    @ApiResponse(responseCode = "400", description = "Nome da cozinha inválido", content = @Content(
+            mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = Problem.class)))
+    ResponseEntity<Boolean> existePorNome(@ApiParam(value = "Nome de uma cozinha", example = "Brasileira") String nome);
+
+    @ApiOperation("Cadastra uma cozinha")
+    @ApiResponse(responseCode = "201", description = "Cozinha cadastrada", content = @Content(
+            schema = @Schema(implementation = Problem.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
+    CozinhaDto adicionar(@ApiParam(name = "corpo", value = "Representação de uma nova cidade") CozinhaInputDto cozinhaInput);
+
+    @ApiOperation("Atualiza uma cozinha")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Cozinha atualizada", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Cozinha não encontrada", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            ))
+    })
+    CozinhaDto atualizar(@ApiParam("ID de uma cidade") Long id,
+                         @ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados") CozinhaInputDto cozinhaInput);
+
+    @ApiOperation("Remove uma cozinha")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Cozinha removida", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Cozinha não encontrada", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)
+            ))
+    })
+    void remover(@ApiParam("ID de uma cidade") Long id);
+
+}
